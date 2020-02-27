a4dc8fb2efca517399ac4a7eae4eaf0a577fca99
inesantos29 <ines.arts@hotmail.com>
2020-02-27 11:55:19 +0000

    Pagination component - function to change page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import data from './data';
 import {Card} from "./components/Card";
@@ -6,47 +6,53 @@ import {NavCategory} from "./components/NavCategory";
 import {SearchInput} from "./components/SearchInput";
 import {Pagination} from "./components/Pagination";
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      data: [],
-      filteredData: [],
-    };
-  }
+const App = () => {
 
-  handleSearch = (search) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [cardsPerPage] = useState(3);
+
+  // Get current cards
+  const indexOfLastCard = currentPage * cardsPerPage;
+  const indexOfFirstCard = indexOfLastCard - cardsPerPage;
+  const currentCards = data.slice(indexOfFirstCard, indexOfLastCard);
+
+  // Change page
+  const paginate = (pageNumber) => (
+   setCurrentPage(pageNumber)
+  );
+
+  return(
+    <div className="flex-container">
+      <NavCategory/>
+      <section className="apps-list">
+        <SearchInput/>
+        {currentCards.map((data) => (
+          <Card
+            key={data.id}
+            id={data.id}
+            name={data.name}
+            description={data.description}
+            categories={data.categories}
+            subscriptions={data.subscriptions}
+          />
+        ))}
+        <Pagination
+          cardsPerPage={cardsPerPage}
+          totalCards={data.length}
+          paginate={paginate}
+        />
+      </section>
+    </div>
+  );
+};
+
+  /*handleSearch = (search) => {
     const normalizedSearch = search.toLowerCase();
     const { data } = this.state;
     const filteredData = search ===  "" ? data : data.filter((data) =>
       data.name.toLowerCase().includes(normalizedSearch) || data.categories.toLowerCase().includes(normalizedSearch)
     );
     this.setState({ filteredData });
-  };
-
-  render(){
-    const {filteredData} = this.state;
-
-    return (
-      <div className="flex-container">
-        <NavCategory/>
-        <section className="apps-list">
-          <SearchInput onChange={this.handleSearch}/>
-          {filteredData.map((data) => (
-            <Card
-              key={data.id}
-              id={data.id}
-              name={data.name}
-              description={data.description}
-              categories={data.categories}
-              subscriptions={data.subscriptions}
-            />
-          ))}
-          <Pagination/>
-        </section>
-      </div>
-    );
-  }
-}
+  };*/
 
 export default App;
--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -1,14 +1,37 @@
 import React from "react";
 
 
-const Pagination = () => (
-  <ul className="pagination">
-    <li><a href="#">&lt;</a></li>
-    <li><a href="#">1</a></li>
-    <li className="active"><a href="#">2</a></li>
-    <li><a href="#">3</a></li>
-    <li><a href="#">&gt;</a></li>
-  </ul>
-);
+const Pagination = ({cardsPerPage, totalCards, paginate}) => {
+
+  const pageNumbers = [];
+
+  for(let i = 1; i <= Math.ceil(totalCards / cardsPerPage); i++) {
+    pageNumbers.push(i);
+  }
+
+  return(
+    <ul className="pagination">
+      {pageNumbers.map(number => {
+
+        return(
+          <li key={number}>
+            <a onClick={() => paginate(number)} href="#">
+              {number}
+            </a>
+          </li>
+        )
+
+      })}
+
+      {/*
+      <li><a href="#">1</a></li>
+      <li className="active"><a href="#">2</a></li>
+      <li><a href="#">3</a></li>
+      <li><a href="#">&gt;</a></li>
+      */}
+    </ul>
+  )
+
+};
 
 export default Pagination;
